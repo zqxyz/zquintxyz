aaa78dca1e03b1793186101ad15603c55a696f0d
Zach Quint <code@zquint.xyz>
2023-01-15 12:49:26 -0500

    posts service fetch

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, onMounted, computed } from 'vue';
+import { ref, onMounted } from 'vue';
 import { postsService } from '../services/posts.service';
 
 interface Post {
